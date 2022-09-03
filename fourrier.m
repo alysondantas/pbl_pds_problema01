17ec5e60eb05c03bfcc4faccaccdd98955fc6d02
Alyson Dantas <alyson.lipe@gmail.com>
2022-09-03 15:44:17 -0300

    Faltando analise das fases

---
+++ fourrier.m
@@ -0,0 +1,18 @@
+% Cálculo da FFT de um sinal no domínio do tempo.
+%	    Sintaxe: [y] = my_fft(t,x,fs)
+%             Entradas:
+%                 t  = vetor de tempo 
+%                 x  = amostras do sinal
+%                 fs = frequência de amostragem do sinal.
+%             Saídas:
+%                 f = vetor de frequências
+%                 Y = valores do módulo de X(jW)
+function [f,Y]  = fourrier(x,fs)
+X          = fft(x);
+Y          = fftshift(X);
+N          = length(x);
+fshift     = (-N/2:N/2-1)*(fs/N);
+powershift = abs(Y)/(N/2);
+f = fshift;
+Y = powershift; 
+return
